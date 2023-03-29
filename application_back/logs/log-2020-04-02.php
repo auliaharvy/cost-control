0a5e9b80aba0b31682088a561a2016cf61f091a6
Muhammad Ramdhan Aditya S <mramdhanass@gmail.com>
2023-03-29 14:00:03 +0700

    penghapusan file tak perlu

--- application_back/logs/log-2020-04-02.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2020-04-02 12:04:01 --> Severity: Notice --> Undefined variable: data D:\xampp\htdocs\Costcontrol\application\views\project\detail.php 118
-ERROR - 2020-04-02 12:04:01 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\Costcontrol\application\views\project\detail.php 118
--- application_back/logs/log-2020-04-25.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2020-04-25 00:28:56 --> 404 Page Not Found: Wp-admin/admin-ajax.php
-ERROR - 2020-04-25 10:32:41 --> 404 Page Not Found: Robotstxt/index
-ERROR - 2020-04-25 10:32:42 --> 404 Page Not Found: Sitemapxml/index
-ERROR - 2020-04-25 10:32:45 --> 404 Page Not Found: Adstxt/index
-ERROR - 2020-04-25 11:44:15 --> 404 Page Not Found: Wp-loginphp/index
